ef5e40986bdf0ad3d87a4f68e8b923a05aa67737
detcelfer <publicmask1@gmail.com>
2023-01-17 00:10:46 +0300

    class in class

--- classes.py
+++ classes.py
@@ -19,21 +19,20 @@ class Table:
         self.schema = schema
         self.c_id = Column('id', 'integer', True)
         self.c_time = Column('timestamp', 'bigint', False)
-
         for c in column_list_type1:
             setattr(self, f'c_{c}', Column(c, 'Numeric', False))
 
     def display(self):
         for attribute, value in self.__dict__.items():
+            # if attribute[0] != 'c':
             print(attribute, '=', value)
+
 
 
 column_list = ['col1', 'col2', 'col3']
 table = Table('tablename', 'sche1', column_list)
 
 table.display()
-# print(table.c_col1.c_name
-table.c_col1.name = '123'
 table.c_col1.display()
-print(isinstance(table.c_col1, Column))
+
 
