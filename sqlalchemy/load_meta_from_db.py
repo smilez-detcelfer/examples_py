fb9c61e41ff1cee76d03d25ac0588c20c2fde2ef
detcelfer <publicmask1@gmail.com>
2023-01-23 23:31:59 +0300

    wft idn

--- sqlalchemy/load_meta_from_db.py
+++ sqlalchemy/load_meta_from_db.py
@@ -6,7 +6,9 @@ engine = create_engine("postgresql://postgres:postgres@localhost:5432/testdb", e
 # кладем в Meta метаданные подгруженные из БД
 meta = MetaData(engine)
 # описываем какие таблицы хотим выгрузить из meta
-coins = Table('example_coins', meta, autoload=True)
+#coins = Table('example_coins', meta, autoload=True)
+for t in tablelist:
+    Table((t, meta, autoload=True))
 
 # можем посмотреть метаданные из подгруженной таблицы, например данные колонок
 coins_cols = coins.c
