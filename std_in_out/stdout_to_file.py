bd70c99fc60b4e4e2f93ddf6e4272f29f942cea0
detcelfer <detcelfer.tech@pm.me>
2023-01-27 13:19:12 +0300

    commit after rename

--- std_in_out/stdout_to_file.py
+++ std_in_out/stdout_to_file.py
@@ -1,13 +1,13 @@
 import sys
 import subprocess
 
-p = subprocess.Popen(['ping', '1.1.1.1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-with open('log', 'w') as sys.stdout:
+p = subprocess.Popen(['ping', '1.1.1.1', '-t'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    while True:
-        line = p.stdout.readline()
-        if not line:
-            break
+while True:
+    line = p.stdout.readline()
+    if not line:
+        break
+    with open('log.txt', 'a') as sys.stdout:
         print(f'string: {line.rstrip().decode()}')
 
 # or just python script.py > log
